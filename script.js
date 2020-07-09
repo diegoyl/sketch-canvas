78fa27e8f225c90b28b6c02cf46e2d9d22e4de6c
diegoyl <67990128+diegoyl@users.noreply.github.com>
2020-07-08 23:47:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,169 @@
+var canvas, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+    state1 = 0;
+    state2 = 0;
+    state3 = 0;
+    coords_list = [];
+
+var x = "black",
+    y = 4;
+
+var blankSketch = true;
+
+function init() {
+     canvas = document.getElementById('myCanvas');
+     ctx = canvas.getContext("2d");
+     w = canvas.width;
+     h = canvas.height;
+
+    // used this Event Listener to help fix mouse position issues
+    canvas.addEventListener("mousemove", function (e) {
+        var mouseXX = e.clientX;
+        var mouseYY = e.clientY;
+        var status = document.getElementById('status');
+        status.innerHTML = mouseXX+" | "+mouseYY;
+    });
+
+     canvas.addEventListener("mousemove", function (e) {
+         findxy('move', e)
+     }, false);
+     canvas.addEventListener("mousedown", function (e) {
+         findxy('down', e)
+     }, false);
+     canvas.addEventListener("mouseup", function (e) {
+         findxy('up', e)
+     }, false);
+     canvas.addEventListener("mouseout", function (e) {
+         findxy('out', e)
+     }, false);
+ }
+
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.lineCap = "round";
+    ctx.stroke();
+    ctx.closePath();
+}
+   
+// clears only the current sketch
+function erase() {
+    ctx.clearRect(0, 0, w, h);
+    blankSketch = true;
+}
+
+var addCount = 0 // keeps track of how many sketches have been added so it can't be pressed once it reaches max.
+
+// clears the current sketch and removes all added sketches
+function eraseAll() {
+    var m = confirm("Want to clear all your sketches?");
+    if (m) {
+        ctx.clearRect(0, 0, w, h);
+        sketch1.src = "https://i.ya-webdesign.com/images/blank-png-1.png";
+        sketch2.src = "https://i.ya-webdesign.com/images/blank-png-1.png";
+        sketch3.src = "https://i.ya-webdesign.com/images/blank-png-1.png";
+        sketch4.src = "https://i.ya-webdesign.com/images/blank-png-1.png";
+        addCount = 0
+    }
+}
+
+// adds picture of sketch to the interpolation bar and creates blank canvas
+function addSketch() {
+    canvas = document.getElementById('myCanvas');
+    let dataURI = canvas.toDataURL();
+    console.log(addCount);
+    if (blankSketch == false) {
+        if (addCount == 0) {
+            sketch1.src = dataURI
+        }
+        else if (addCount == 1) {
+            sketch2.src = dataURI
+        }
+        if (addCount == 2) {
+            sketch3.src = dataURI
+        }
+        else if (addCount == 3) {
+            sketch4.src = dataURI
+        }
+        else if (addCount > 3) {
+            alert('You can only add up to 4 sketches. Press "Clear All" to start over.')
+        }
+        erase();
+        blankSketch = true;
+        addCount = addCount + 1;
+    }
+    else if (blankSketch == true) {
+        alert("You can't add an empty sketch")
+    }
+
+}
+
+function downloadObjectAsJson(exportObj, exportName){
+  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+  var downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute("href",     dataStr);
+  downloadAnchorNode.setAttribute("download", exportName + ".json");
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
+// Function for saving array - for mouse coordinates see prevX/Y and currX/Y
+function save() {
+
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(coords_list));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", "test.json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+
+
+function findxy(res, e) {
+
+  prevX = currX;
+  prevY = currY;
+  currX = e.clientX - canvas.offsetLeft;
+  currY = e.clientY - canvas.offsetTop;
+  mouseX = currX - prevX;
+  mouseY = currY - prevY;
+
+    if (res == 'down') {
+
+        flag = true;
+        dot_flag = true;
+        blankSketch = false;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+            state1 = 1;
+            state2 = 0;
+        }
+    }
+    if (res == 'up' || res == "out") {
+        flag = false;
+        state1 = 0;
+        state2 = 1;
+    }
+    if (res == 'move') {
+        if (flag) {
+            draw();
+
+        }
+    }
+    coords_list.push([mouseX, mouseY, state1, state2, state3]);
+    // coords_list.push([currX,currY,prevX,prevY]);
+
+}
